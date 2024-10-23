6251d346db7473d6d8c64f7a863258253f6a4a7f
TechwariumDeveloper <techwariumdev@gmail.com>
2024-10-23 17:33:14 +0530

    style view button nd inside view button create formCheck nd explanation buuton

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,12 @@
 export default function Footer() {
   return (
-    <footer className="text-center mt-8">
-      <button className="text-white underline">Support</button>
-      <br />
-      <button className="text-white underline mt-2">Back to Top</button>
-      <div className="mt-4 text-white">Made with ♥</div>
-    </footer>
+    <>
+      <footer className="text-center mt-8">
+        <button className="text-white underline">Support</button>
+        <br />
+        <button className="text-white underline mt-2">Back to Top</button>
+        <div className="mt-4 text-white">Made with ♥</div>
+      </footer>
+    </>
   );
 }
